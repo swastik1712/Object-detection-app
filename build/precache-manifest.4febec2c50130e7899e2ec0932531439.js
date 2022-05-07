2afda75adc64df4985007d4f5e300f3ffab1f4a9
swastik1712 <58765479+swastik1712@users.noreply.github.com>
2022-05-07 20:50:13 +0530

    Add files via upload

---
+++ build/precache-manifest.4febec2c50130e7899e2ec0932531439.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90dc78607e95565990bdf4ea85dfcc54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0972c4dd202c0dc4b01d",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "e93fab106f98173be9c6",
+    "url": "/static/js/2.4ecb0673.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/static/js/2.4ecb0673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0972c4dd202c0dc4b01d",
+    "url": "/static/js/main.fd1a06f9.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
